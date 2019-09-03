12e5239ed5497d0a2a808bdc4439881f72207d96
Vinicius Fraga <vinifragam@gmail.com>
2019-09-03 15:50:55 -0300

    Middleware de autenticacao

--- node/modulo02/src/app/controllers/UserController.js
+++ node/modulo02/src/app/controllers/UserController.js
@@ -11,6 +11,12 @@ class UserController {
 
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
