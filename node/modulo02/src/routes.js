fd14029c9b19eaf9b9ae7c472aa7e7b929678e9b
Vinicius Fraga <vinifragam@gmail.com>
2019-09-03 14:36:14 -0300

    Cadastro de usuarios

--- node/modulo02/src/routes.js
+++ node/modulo02/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Vinicius Fraga',
-    email: 'vinifragam@gmail.com',
-    password_hash: '1234',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
