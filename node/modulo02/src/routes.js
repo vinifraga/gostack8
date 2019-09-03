e1a60f0c06c471d3344859a91f080f09d13ff53a
Vinicius Fraga <vinifragam@gmail.com>
2019-09-03 14:15:51 -0300

    Criando loader de models

--- node/modulo02/src/routes.js
+++ node/modulo02/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocketseat' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Vinicius Fraga',
+    email: 'vinifragam@gmail.com',
+    password_hash: '1234',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
