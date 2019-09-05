ae4644b7869d0fadc9427da494bb28e45324fb96
Vinicius Fraga <vinifragam@gmail.com>
2019-09-04 21:51:07 -0300

    User: update, session: update. authMiddleware

---
+++ node/desafio02/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '7f5947135737adc4302524feb01bd7f7',
+  expiresIn: '7d',
+};
--- node/desafio02/src/routes.js
+++ node/desafio02/src/routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/user', UserController.store);
-// routes.post('/session', SessionController.store);
-
-// routes.put('/user', SessionController.update);
+routes.post('/session', SessionController.store);
+routes.use(authMiddleware);
+routes.put('/user', UserController.update);
 
 export default routes;
