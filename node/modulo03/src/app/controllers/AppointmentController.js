633b930fb8fcb6ae11ccde2fae4323e37782d2fa
Vinicius Fraga <vinifragam@gmail.com>
2019-09-06 19:44:36 -0300

    Aplicando paginação

--- node/modulo03/src/app/controllers/AppointmentController.js
+++ node/modulo03/src/app/controllers/AppointmentController.js
@@ -6,9 +6,13 @@ import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
