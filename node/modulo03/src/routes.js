1a3c2790353e6dc9f3341548c6ccba030df11a5f
Vinicius Fraga <vinifragam@gmail.com>
2019-09-07 11:05:49 -0300

    Listando agenda do prestador

--- node/modulo03/src/routes.js
+++ node/modulo03/src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,5 +23,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
