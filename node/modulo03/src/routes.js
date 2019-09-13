d002aadc47e51c7a9e52c2c86af57e726a5dea36
Vinicius Fraga <vinifragam@gmail.com>
2019-09-13 11:33:12 -0300

    Monit. falhas na fila/List. horar. disp.

--- node/modulo03/src/routes.js
+++ node/modulo03/src/routes.js
@@ -12,16 +12,20 @@ import NotificationController from './app/controllers/NotificationController';
 import authMiddleware from './app/middlewares/auth';
 
 import multerConfig from './config/multer';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
