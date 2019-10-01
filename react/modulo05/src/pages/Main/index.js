5cd7198eefa678013bab898f69b06efb8367f6a9
Vinicius Fraga <vinifragam@gmail.com>
2019-10-01 11:37:06 -0300

    Exibindo repositório

--- react/modulo05/src/pages/Main/index.js
+++ react/modulo05/src/pages/Main/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 import api from '../../services/api';
 
 export default class Main extends Component {
--- react/modulo05/src/pages/Repository/index.js
+++ react/modulo05/src/pages/Repository/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Loading, Owner } from './styles';
 import api from '../../services/api';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -42,6 +44,21 @@ export default class Repository extends Component {
   }
 
   render() {
-    return <h1>Repository:</h1>;
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
   }
 }
