d34072bcefeb2218c6d529e09e7be04e45f3ca03
Vinicius Fraga <vinifragam@gmail.com>
2019-10-01 10:51:52 -0300

    Navegação de rotas

--- react/modulo05/src/pages/Main/index.js
+++ react/modulo05/src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import { Container, Form, SubmitButton, List } from './styles';
 import api from '../../services/api';
@@ -82,7 +83,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
