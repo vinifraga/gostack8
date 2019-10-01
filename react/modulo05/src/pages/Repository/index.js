ea973db532c749d7e83f829bf53e8d36e1c30ad3
Vinicius Fraga <vinifragam@gmail.com>
2019-10-01 11:06:18 -0300

    Carregando dados da API

--- react/modulo05/src/pages/Repository/index.js
+++ react/modulo05/src/pages/Repository/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      await api.get(`repos/${repoName}`),
+      await api.get(`repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    return <h1>Repository:</h1>;
+  }
 }
