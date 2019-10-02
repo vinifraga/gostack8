82d5f03d504e31f5ebd6349b37dead4e78a31ceb
Vinicius Fraga <vinifragam@gmail.com>
2019-10-02 13:00:04 -0300

    Styled Components

--- native/modulo06/src/pages/Main/index.js
+++ native/modulo06/src/pages/Main/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
-  return (
-    <View>
-      <Text>Oi</Text>
-    </View>
-  );
+  return <Container />;
 }
 
 Main.navigationOptions = {
