852cc82d4d7c3d6e88d384cbc90c25108a551397
Vinicius Fraga <vinifragam@gmail.com>
2019-10-02 14:02:04 -0300

    Estilizando formulário

--- native/modulo06/src/pages/Main/index.js
+++ native/modulo06/src/pages/Main/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Input, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
