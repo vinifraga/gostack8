3e7b3b3c234f1e7059b02cbb36f4c057788b237e
Vinicius Fraga <vinifragam@gmail.com>
2019-10-03 14:43:12 -0300

    Estilos globais

--- react/modulo07/src/App.js
+++ react/modulo07/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 // import { Container } from './styles';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       {/* <Header /> */}
       <Routes />
     </BrowserRouter>
