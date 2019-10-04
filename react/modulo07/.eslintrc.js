4b8b99a8a78cd52d3093ebfde41ee6334e157c32
Vinicius Fraga <vinifragam@gmail.com>
2019-10-04 16:10:47 -0300

    Reactotron + Redux

--- react/modulo07/.eslintrc.js
+++ react/modulo07/.eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
-    "import/prefer-default-export": "off",
-    "react/state-in-constructor": "off",
-    "react/static-property-placement": ["off", "property assignment"],
-    "react/static-property-placement": ["error", "static public field"]
-  }
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': ['off', 'property assignment'],
+    'react/static-property-placement': ['error', 'static public field'],
+    'no-console': ['error', { allow: ['tron'] }],
+  },
 };
--- react/modulo07/src/App.js
+++ react/modulo07/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/reactotronConfig';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 import Header from './components/Header';
-
 import store from './store';
 
 // import { Container } from './styles';
