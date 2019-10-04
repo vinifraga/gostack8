543a272c3e4ddf4d7e6ee2723531723f5d026cd9
Vinicius Fraga <vinifragam@gmail.com>
2019-10-04 16:35:13 -0300

    Remover produto

--- react/modulo07/src/pages/Cart/index.js
+++ react/modulo07/src/pages/Cart/index.js
@@ -6,9 +6,10 @@ import {
 } from 'react-icons/md';
 import { connect } from 'react-redux';
 
+import { dispatch } from 'rxjs/internal/observable/range';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -46,7 +47,12 @@ function Cart({ cart }) {
                 <strong>R$258,80</strong>
               </td>
               <td>
-                <button type="button">
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
                   <MdDelete size={20} color="#7159c1" />
                 </button>
               </td>
--- react/modulo07/src/store/modules/cart/reducer.js
+++ react/modulo07/src/store/modules/cart/reducer.js
@@ -12,6 +12,14 @@ export default function cart(state = [], action) {
           draft.push({ ...action.product, amount: 1 });
         }
       });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
     default:
       return state;
   }
