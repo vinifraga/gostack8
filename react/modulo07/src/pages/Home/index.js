f3f0c49eb4faa23f0cfdcac45fa91ece8133d16c
Vinicius Fraga <vinifragam@gmail.com>
2019-10-04 09:59:41 -0300

    Estilização da Home

---
+++ react/modulo07/src/pages/Home/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
+          alt="Chuteira"
+        />
+        <strong>Chuteira Nike Mercurial Superfly 7</strong>
+        <span>R$529,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
+          alt="Chuteira"
+        />
+        <strong>Chuteira Nike Mercurial Superfly 7</strong>
+        <span>R$529,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
+          alt="Chuteira"
+        />
+        <strong>Chuteira Nike Mercurial Superfly 7</strong>
+        <span>R$529,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
+          alt="Chuteira"
+        />
+        <strong>Chuteira Nike Mercurial Superfly 7</strong>
+        <span>R$529,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
+          alt="Chuteira"
+        />
+        <strong>Chuteira Nike Mercurial Superfly 7</strong>
+        <span>R$529,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
+          alt="Chuteira"
+        />
+        <strong>Chuteira Nike Mercurial Superfly 7</strong>
+        <span>R$529,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
--- react/modulo07/src/routes.js
+++ react/modulo07/src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './Home';
-import Cart from './Cart';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 export default function Routes() {
   return (
