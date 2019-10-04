f534173291c3eed7a6d86456763f0ac6de5589ed
Vinicius Fraga <vinifragam@gmail.com>
2019-10-04 17:00:26 -0300

    Alterando quantidade

--- react/modulo07/src/pages/Cart/index.js
+++ react/modulo07/src/pages/Cart/index.js
@@ -10,7 +10,15 @@ import { bindActionCreators } from 'redux';
 import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+  function increment(product) {
+    updateAmount(product.id, product.amount + 1);
+  }
+
+  function decrement(product) {
+    updateAmount(product.id, product.amount - 1);
+  }
+
   return (
     <Container>
       <ProductTable>
@@ -35,11 +43,11 @@ function Cart({ cart, removeFromCart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button type="button" onClick={() => decrement(product)}>
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
                   <input type="text" readOnly value={product.amount} />
-                  <button type="button">
+                  <button type="button" onClick={() => increment(product)}>
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
                 </div>
--- react/modulo07/src/store/modules/cart/actions.js
+++ react/modulo07/src/store/modules/cart/actions.js
@@ -8,3 +8,7 @@ export function addToCart(product) {
 export function removeFromCart(id) {
   return { type: '@cart/REMOVE', id };
 }
+
+export function updateAmount(id, amount) {
+  return { type: '@cart/UPDATE_AMOUNT', id, amount };
+}
