638e845d9a6cb54742f75f9f7338f1d6fdae94e9
Vinicius Fraga <vinifragam@gmail.com>
2019-10-04 10:48:41 -0300

    Buscando produtos da API

--- react/modulo07/src/pages/Home/index.js
+++ react/modulo07/src/pages/Home/index.js
@@ -1,107 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
-          alt="Chuteira"
-        />
-        <strong>Chuteira Nike Mercurial Superfly 7</strong>
-        <span>R$529,99</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
-          alt="Chuteira"
-        />
-        <strong>Chuteira Nike Mercurial Superfly 7</strong>
-        <span>R$529,99</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    this.setState({ products: data });
+  }
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
-          alt="Chuteira"
-        />
-        <strong>Chuteira Nike Mercurial Superfly 7</strong>
-        <span>R$529,99</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormated}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
-          alt="Chuteira"
-        />
-        <strong>Chuteira Nike Mercurial Superfly 7</strong>
-        <span>R$529,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
-          alt="Chuteira"
-        />
-        <strong>Chuteira Nike Mercurial Superfly 7</strong>
-        <span>R$529,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-nike-mercurial-superfly-7-academy-tf/58/HZM-1912-058/HZM-1912-058_zoom1.jpg?ims=120x"
-          alt="Chuteira"
-        />
-        <strong>Chuteira Nike Mercurial Superfly 7</strong>
-        <span>R$529,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
