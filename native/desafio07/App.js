651e5119cac87e2f45f8a4bcb7eeab4ad42bd146
Vinicius Fraga <vinifragam@gmail.com>
2019-10-06 09:42:26 -0300

    Iniciando desafio07

---
+++ native/desafio07/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="ligth-content" />
+      <View>
+        <Text>Desafio 07</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
