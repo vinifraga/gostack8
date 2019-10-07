32c246df43c5806b0009ba29b6a0dbac085da4c4
Vinicius Fraga <vinifragam@gmail.com>
2019-10-07 13:39:21 -0300

    Hook useMemo

--- react/modulo08/src/App.js
+++ react/modulo08/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
   const [tech, setTech] = useState([]);
@@ -21,6 +21,8 @@ function App() {
     localStorage.setItem('tech', JSON.stringify(tech));
   }, [tech]);
 
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <>
       <ul>
@@ -28,6 +30,7 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
       <input
         type="text"
         value={newTech}
