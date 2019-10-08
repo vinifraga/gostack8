b718ce2b93fcca9f727df4994d32d99b22f403ab
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 13:31:51 -0300

    Utilizando Root Import

--- react/modulo09/.eslintrc.js
+++ react/modulo09/.eslintrc.js
@@ -28,4 +28,11 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-props-no-spreading': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
--- react/modulo09/src/routes/Route.js
+++ react/modulo09/src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
