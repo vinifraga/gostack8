e82ac1ef75c47a04f1af0c5980b55ed856cc05ea
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 15:42:14 -0300

    Autenticação

--- react/modulo09/src/App.js
+++ react/modulo09/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
-import './config/ReactotronConfig';
 
 import store from './store';
 
--- react/modulo09/src/pages/SignIn/index.js
+++ react/modulo09/src/pages/SignIn/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
+import { signInRequest } from '~/store/modules/auth/actions';
 import logo from '~/assets/logo.svg';
 
 const schema = Yup.object().shape({
@@ -13,9 +15,12 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
