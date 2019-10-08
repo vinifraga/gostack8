7f25853bd90468bcd43411676847c711809072a2
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 20:42:23 -0300

    Estilizando notificações

--- react/modulo09/src/components/Header/index.js
+++ react/modulo09/src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Notifications from '~/components/Notifications';
+
 import { Container, Content, Profile } from './styles';
 import logoHeader from '~/assets/logoHeader.svg';
 
@@ -14,6 +16,7 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Diego Fernandes</strong>
