c12fbd6278c3045c8e9fa48bc302edd4bdf617a6
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 13:57:30 -0300

    Estilização da autenticação

--- react/modulo09/src/pages/SignUp/index.js
+++ react/modulo09/src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
