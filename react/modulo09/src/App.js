8261243bea518554b821a5ad85a8e2e36141987a
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 13:21:55 -0300

    Estilos globais

--- react/modulo09/src/App.js
+++ react/modulo09/src/App.js
@@ -3,11 +3,13 @@ import { Router } from 'react-router-dom';
 
 import history from './services/history';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 import './config/ReactotronConfig';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
