498516984154d70550cf84a5d278052d22fbc2c6
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 11:45:37 -0300

    Configurando Reactotron

--- react/modulo09/src/App.js
+++ react/modulo09/src/App.js
@@ -3,6 +3,7 @@ import { Router } from 'react-router-dom';
 
 import history from './services/history';
 import Routes from './routes';
+import './config/ReactotronConfig';
 
 function App() {
   return (
