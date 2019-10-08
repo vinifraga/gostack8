4477171064766d549fc7c866f3b5c7417bc901f4
Vinicius Fraga <vinifragam@gmail.com>
2019-10-08 18:41:13 -0300

    Loading da autenticação

--- react/modulo09/src/store/modules/auth/sagas.js
+++ react/modulo09/src/store/modules/auth/sagas.js
@@ -1,24 +1,28 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 
 import api from '~/services/api';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, `sessions`, { email, password });
+    const response = yield call(api.post, `sessions`, { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário não é prestador');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
