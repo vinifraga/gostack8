809f920140bc681474e1e1bae005c5f364622c74
Vinicius Fraga <vinifragam@gmail.com>
2019-10-09 00:36:00 -0300

    Estilização do Dashboard

--- react/modulo09/src/pages/Dashboard/index.js
+++ react/modulo09/src/pages/Dashboard/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time available>
+          <strong>08:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
