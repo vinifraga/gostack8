aa4721372daccca0cedb446f146ee7d0ff84d876
Vinicius Fraga <vinifragam@gmail.com>
2019-10-18 18:02:42 -0300

    Configurando StatusBar

--- native/modulo10/src/index.js
+++ native/modulo10/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-// import { Container } from './styles';
 import Routes from './routes';
 
 export default function src() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
