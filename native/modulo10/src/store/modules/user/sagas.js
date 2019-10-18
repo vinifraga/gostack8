1be2394e7cf863bbfaedc47bd7acb1ab67485e9b
Vinicius Fraga <vinifragam@gmail.com>
2019-10-18 19:08:08 -0300

    Configurando Redux

---
+++ native/modulo10/src/store/modules/user/sagas.js
@@ -0,0 +1,32 @@
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    Alert.alert(
+      'Falha na atualização',
+      'Houve um erro na atualização do perfil, verifique seus dados'
+    );
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
