e291a207514d084a26a0f3ca562b095793c24ab7
Vinicius Fraga <vinifragam@gmail.com>
2019-10-18 15:53:44 -0300

    Configurando background

--- native/modulo10/src/pages/SignIn/index.js
+++ native/modulo10/src/pages/SignIn/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
-    <View>
+    <Background>
       <Text>SignIn</Text>
-    </View>
+    </Background>
   );
 }
