2fb89116f7232f5c6a74e14758fe2aa2e2686ffe
Vinicius Fraga <vinifragam@gmail.com>
2019-10-18 16:14:17 -0300

    Input & Button

--- native/modulo10/src/pages/SignIn/index.js
+++ native/modulo10/src/pages/SignIn/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 // import { Container } from './styles';
 
@@ -9,6 +11,13 @@ export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        icon="call"
+        placeholder="Digite seu nome"
+        style={{ marginTop: 30 }}
+      />
+      <Button>Entrar</Button>
     </Background>
   );
 }
