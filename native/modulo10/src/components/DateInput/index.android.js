b49a0d9e131ce0f84dd03ee85fac7a547327449f
Vinicius Fraga <vinifragam@gmail.com>
2019-10-19 12:08:56 -0300

    DatePicker por plataforma

---
+++ native/modulo10/src/components/DateInput/index.android.js
@@ -0,0 +1,37 @@
+import React, { useMemo } from 'react';
+import { DatePickerAndroid } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import { Container, DateButton, DateText } from './styles';
+
+export default function DateInput({ date, onChange }) {
+  const dateFormated = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
+
+  async function handleOpenPicker() {
+    const { action, year, month, day } = await DatePickerAndroid.open({
+      mode: 'calendar',
+      date,
+      minDate: new Date(),
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+      const selectedDate = new Date(year, month, day);
+
+      onChange(selectedDate);
+    }
+  }
+
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <Icon name="event" color="#FFF" size={20} />
+        <DateText>{dateFormated}</DateText>
+      </DateButton>
+    </Container>
+  );
+}
--- native/modulo10/src/pages/New/SelectDateTime/index.js
+++ native/modulo10/src/pages/New/SelectDateTime/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { TouchableOpacity } from 'react-native';
 
 import { Container } from './styles';
 import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
 
 export default function SelectDateTime() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <Background>
-      <Container />
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
     </Background>
   );
 }
 
-SelectDateTime.navigationOptions = {
+SelectDateTime.navigationOptions = ({ navigation }) => ({
   title: 'Selecione o horário',
-};
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
