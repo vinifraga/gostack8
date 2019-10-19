35ad5cd15d3e69a00265ef0f596ff8294a01b7bc
Vinicius Fraga <vinifragam@gmail.com>
2019-10-19 11:28:10 -0300

    Listagem de prestadores

--- native/modulo10/src/pages/New/SelectDateTime/index.js
+++ native/modulo10/src/pages/New/SelectDateTime/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+import Background from '~/components/Background';
 
 export default function SelectDateTime() {
-  return <View />;
+  return (
+    <Background>
+      <Container />
+    </Background>
+  );
 }
+
+SelectDateTime.navigationOptions = {
+  title: 'Selecione o horário',
+};
--- native/modulo10/src/pages/New/SelectProvider/index.js
+++ native/modulo10/src/pages/New/SelectProvider/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { View, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+
 import Background from '~/components/Background';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
-export default function SelectProvider() {
-  return <Background />;
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() => {
+                navigation.navigate('SelectDateTime', { provider });
+              }}
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url.replace('localhost', '192.168.0.15')
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
