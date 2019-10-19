7df43bb52827a3f88e6cc970d854502b497706ba
Vinicius Fraga <vinifragam@gmail.com>
2019-10-19 09:52:44 -0300

    Atualização de perfil

--- native/modulo10/src/store/modules/user/sagas.js
+++ native/modulo10/src/store/modules/user/sagas.js
@@ -6,16 +6,16 @@ import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
-      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
 
     const response = yield call(api.put, 'users', profile);
+
     Alert.alert('Sucesso', 'Perfil atualizado com sucesso');
 
     yield put(updateProfileSuccess(response.data));
