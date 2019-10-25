34650e17727ca8a8adb9f552ac4b8f33190e5e44
Vinicius Fraga <vinifragam@gmail.com>
2019-10-25 17:18:01 -0300

    Mock do useSelector

--- react/modulo12/src/__tests__/components/TechList.test.js
+++ react/modulo12/src/__tests__/components/TechList.test.js
@@ -1,41 +1,24 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import TechList from '~/components/TechList';
 
+jest.mock('react-redux');
+
 describe('TechList component', () => {
-  beforeEach(() => {
-    localStorage.clear();
-  });
-
-  it('should be able to add new tech', () => {
-    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
-
-    fireEvent.change(getByLabelText('Tech'), {
-      target: { value: 'Node.js' },
-    });
-    fireEvent.submit(getByTestId('tech-form'));
-
-    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
-    expect(getByLabelText('Tech')).toHaveValue('');
-  });
-
-  it('should store techs in storage', () => {
-    let { getByTestId, getByLabelText, getByText } = render(<TechList />);
-
-    fireEvent.change(getByLabelText('Tech'), {
-      target: { value: 'Node.js' },
-    });
-    fireEvent.submit(getByTestId('tech-form'));
-
-    cleanup();
-
-    ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
-
-    expect(localStorage.setItem).toHaveBeenCalledWith(
-      'techs',
-      JSON.stringify(['Node.js'])
+  it('should render tech list', () => {
+    useSelector.mockImplementation(cb =>
+      cb({
+        techs: ['Node.js', 'ReactJS'],
+      })
     );
+
+    const { getByText, getByTestId, debug } = render(<TechList />);
+
+    debug();
+
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
   });
 });
--- react/modulo12/src/components/TechList/index.js
+++ react/modulo12/src/components/TechList/index.js
@@ -1,26 +1,17 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 // import { Container } from './styles';
 
 export default function TechList() {
-  const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
 
-  useEffect(() => {
-    const storageTechs = localStorage.getItem('techs');
-
-    if (storageTechs) {
-      setTechs(JSON.parse(storageTechs));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('techs', JSON.stringify(techs));
-  }, [techs]);
+  const techs = useSelector(state => state.techs);
+  const dispatch = useDispatch();
 
   function handleAddTech() {
-    setTechs([...techs, newTech]);
+    dispatch({ type: 'ADD_TECH', payload: { tech: newTech } });
     setNewTech('');
   }
 
