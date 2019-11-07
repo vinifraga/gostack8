0afd14d3cb32d188eed2f71c7b4270c93eaa1384
Vinicius Fraga <vinifragam@gmail.com>
2019-11-07 18:36:27 -0300

    Relacionamentos

--- bonus/gonode/app/Models/User.js
+++ bonus/gonode/app/Models/User.js
@@ -14,7 +14,7 @@ class User extends Model {
      * A hook to hash the user password before saving
      * it to the database.
      */
-    this.addHook('beforeSave', async (userInstance) => {
+    this.addHook('beforeSave', async userInstance => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
@@ -33,6 +33,14 @@ class User extends Model {
    */
   tokens () {
     return this.hasMany('App/Models/Token')
+  }
+
+  projects () {
+    return this.hasMany('App/Models/Project')
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Task')
   }
 }
 
