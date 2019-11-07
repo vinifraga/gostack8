3946cd3a7337c2a9a6551f4c0dd09a6ec7072d32
Vinicius Fraga <vinifragam@gmail.com>
2019-11-07 18:01:28 -0300

    Upload de arquivos

---
+++ bonus/gonode/app/Controllers/Http/FileController.js
@@ -0,0 +1,54 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+
+/**
+ * Resourceful controller for interacting with files
+ */
+class FileController {
+  /**
+   * Create/save a new file.
+   * POST files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.moved()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Erro no upload de arquivo' } })
+    }
+  }
+}
+
+module.exports = FileController
--- bonus/gonode/start/routes.js
+++ bonus/gonode/start/routes.js
@@ -21,3 +21,5 @@ Route.post('sessions', 'SessionController.store')
 
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.post('files', 'FileController.store')
