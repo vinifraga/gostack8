eca6d733cb0710c99adb7442338a8673435e71ab
Vinicius Fraga <vinifragam@gmail.com>
2019-11-08 19:33:28 -0300

    Paginação de projetos

--- bonus/gonode/app/Controllers/Http/ProjectController.js
+++ bonus/gonode/app/Controllers/Http/ProjectController.js
@@ -19,10 +19,12 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index ({ request }) {
+    const { page } = request.get()
+
     const projects = await Project.query()
       .with('user')
-      .fetch()
+      .paginate(page)
 
     return projects
   }
