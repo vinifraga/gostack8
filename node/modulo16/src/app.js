b228fbfdd77f2555b0c43fecab923d7069220011
Vinicius Fraga <vinifragam@gmail.com>
2019-11-12 09:10:47 -0300

    Utilizando Rate Limit

--- node/modulo16/src/app.js
+++ node/modulo16/src/app.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import helmet from 'helmet';
 import express from 'express';
 import path from 'path';
@@ -33,6 +36,21 @@ class App {
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
